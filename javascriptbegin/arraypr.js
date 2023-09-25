fdf3bc82bb1500473ed75323a84657afc480fe5f
SourabhIngawale <ingawalesourabh@gmail.com>
2023-09-25 23:56:25 +0530

    array practice

---
+++ javascriptbegin/arraypr.js
@@ -0,0 +1,16 @@
+
+let arr=[34,78,98];
+let myArray=new Array(367,687);
+let car=new Array("SUV,MUV");
+let a="hello maya";
+let arrString=["akash","mayur","abhishek"];
+
+{
+
+console.log(arr);
+console.log(myArray);
+console.log(car);
+console.log(a.replace("maya",arrString[2]));
+console.log(a.indexOf("l"));
+
+}
