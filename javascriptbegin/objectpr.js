fafbec4a967fdf402469279a9dd59e9733a78275
SourabhIngawale <ingawalesourabh@gmail.com>
2023-10-07 13:35:34 +0530

    object practise

---
+++ javascriptbegin/objectpr.js
@@ -0,0 +1,26 @@
+
+let person=
+{
+    name:'sourabh',
+    age : 27,
+    lastname : 'ingawale',
+    Degree : 'BCA',
+
+}
+
+let person1=
+{
+    name:'akash',
+    age : 27,
+    lastname : 'ingawale',
+    Degree : 'BCA',
+    gf: 2,
+
+}
+
+if(person=person1){
+    console.log("Data is correct");
+}
+else{
+    console.log("data is wrong");
+    }
